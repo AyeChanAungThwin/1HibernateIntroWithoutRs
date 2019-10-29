455dc9a04c6908829fa80e34ed40946c8a5598c2
Aye Chan Aung Thwin <ayechanaungthwin@gmail.com>
2019-10-29 17:07:38 +0630

    added DTO

---
+++ src/main/java/acat/hibernate/dto/PersonDto.java
@@ -0,0 +1,53 @@
+package acat.hibernate.dto;
+
+import java.io.Serializable;
+
+import acat.hibernate.entity.Person;
+
+public class PersonDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8915317817868710134L;
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phNo;
+	
+	public PersonDto(Person person) {
+		this.id=person.getId();
+		this.name=person.getName();
+		this.email=person.getEmail();
+		this.phNo=person.getPhNo();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhNo() {
+		return phNo;
+	}
+
+	public void setPhNo(String phNo) {
+		this.phNo = phNo;
+	}
+}
