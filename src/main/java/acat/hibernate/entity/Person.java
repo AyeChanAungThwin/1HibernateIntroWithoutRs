28c5a26f23d656c376dab78e3a66fa6d3f44832a
Aye Chan Aung Thwin <ayechanaungthwin@gmail.com>
2019-10-31 14:45:41 +0630

    updated Person Entity

--- src/main/java/acat/hibernate/entity/Person.java
+++ src/main/java/acat/hibernate/entity/Person.java
@@ -34,6 +34,10 @@ public class Person implements Serializable {
 	public Long getId() {
 		return id;
 	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
